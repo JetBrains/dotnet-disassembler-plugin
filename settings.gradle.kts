2c43843f304f251e42fc8d573d98b61a0e4d082f
smolchanovsky <smolchanovsky@gmail.com>
2026-01-16 17:38:28 +0400

    Run backend tests on Linux

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
         maven { setUrl("https://cache-redirector.jetbrains.com/maven-central") }
         maven { setUrl("https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-eap") }
+        gradlePluginPortal()
     }
     resolutionStrategy {
         eachPlugin {
