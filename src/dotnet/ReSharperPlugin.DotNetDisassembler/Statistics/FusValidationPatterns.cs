49768ae33cabba1dad5d71c3d5e4bc1db5087afc
smolchanovsky <smolchanovsky@gmail.com>
2026-02-13 14:58:56 +0400

    Fix FUS target framework regex to avoid curly brace conflict

--- src/dotnet/ReSharperPlugin.DotNetDisassembler/Statistics/FusValidationPatterns.cs
+++ src/dotnet/ReSharperPlugin.DotNetDisassembler/Statistics/FusValidationPatterns.cs
@@ -7,6 +7,7 @@ public static class FusValidationPatterns
     public const string SdkType = @"Microsoft\.NET\.Sdk(\.[A-Za-z][A-Za-z0-9]*)*";
     public static readonly Regex SdkTypeRegex = new(SdkType, RegexOptions.Compiled);
 
-    public const string TargetFramework = @"(net|netcoreapp|netstandard)\d+\.\d+(-.+)?|net\d{2,3}";
+    // Note: avoid {n,m} quantifiers — curly braces conflict with FUS {regexp:...} delimiter parsing
+    public const string TargetFramework = @"(net|netcoreapp|netstandard)\d+\.\d+(-.+)?|net\d\d\d?";
     public static readonly Regex TargetFrameworkRegex = new(TargetFramework, RegexOptions.Compiled);
 }
